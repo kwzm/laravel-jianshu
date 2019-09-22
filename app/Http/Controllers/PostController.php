558eaf245d0730c5c0164d07f55a3830af6c5f00
kwzm <kwzm93@163.com>
2019-09-22 20:50:04 +0800

    init posts routes and controllers

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PostController extends Controller {
+	// 列表
+	public function index() {
+
+	}
+
+	// 详情页面
+	public function show() {
+
+	}
+
+	// 创建逻辑
+	public function create() {
+
+	}
+
+	// 创建逻辑
+	public function store() {
+
+	}
+
+	// 编辑页面
+	public function edit() {
+
+	}
+
+	// 编辑逻辑
+	public function update() {
+
+	}
+
+	// 删除逻辑
+	public function delete() {
+
+	}
+}
--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,17 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// 文章列表頁
+Route::get('/posts', '\App\Http\Controllers\PostController@index');
+// 文章詳情頁
+Route::get('/posts{post}', '\App\Http\Controllers\PostController@show');
+// 创建文章
+Route::get('/posts/create', '\App\Http\Controllers\PostController@create');
+Route::post('/posts/create', '\App\Http\Controllers\PostController@store');
+// 编辑文章
+Route::get('/posts/{post}/edit', '\App\Http\Controllers\PostController@edit');
+Route::put('/posts/{post}', '\App\Http\Controllers\PostController@update');
+// 删除文章
+Route::get('/posts/delete', '\App\Http\Controllers\PostController@delete');
