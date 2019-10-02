02a05f5b09d59d1fdf172820a25507dff19e6fec
kwzm <kwzm93@163.com>
2019-10-02 18:04:14 +0800

    complete register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \App\User;
 
 class RegisterController extends Controller
 {
@@ -13,6 +14,20 @@ class RegisterController extends Controller
 
     // 注册行为
     public function register(){
+    	// 校验
+    	$this->validate(request(), [
+    		'name' => 'required|min:3|unique:users,name',
+    		'email' => 'required|unique:users,email|email',
+    		'password' => 'required|min:5|max:10|confirmed',
+    	]);
 
+    	// 逻辑
+    	$name = request('name');
+    	$email = request('email');
+    	$password = bcrypt(request('password'));
+    	$user = User::create(compact('name', 'email', 'password'));
+
+    	// 渲染
+    	return redirect('/login');
     }
 }
