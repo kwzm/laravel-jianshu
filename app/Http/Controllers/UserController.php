80e6f5837573e1734400d02fcd4dee07f6ce85e0
kwzm <kwzm93@163.com>
2019-10-02 23:25:00 +0800

    complete avatar

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,11 +8,35 @@ class UserController extends Controller
 {
     // 个人设置页面
     public function setting() {
-    	return view('user.setting');
+    	$user = \Auth::user();
+    	return view('user.setting', compact('user'));
     }
 
     // 个人设置行为
     public function settingStore() {
+    	// 验证
+    	$this->validate(request(), [
+    		'name' => 'required|min:3',
+    	]);
 
+    	// 逻辑
+    	$name = request('name');
+    	$user = \Auth::user();
+    	if ($name != $user->name) {
+    		if (User::where('name', $name) -> count() > 0)	{
+    			return back()->withErrors('用户名已经被注册');
+    		}
+    		$user->name = $name;
+    	}
+
+    	if (request('avatar')) {
+    		$path = request('avatar')->storePublicly($user->id);
+    		$user->avatar = "/storage/" . $path;
+    	}
+
+    	$user->save();
+
+    	// 渲染
+    	return back();
     }
 }
--- public/js/ylaravel.js
+++ public/js/ylaravel.js
@@ -9,3 +9,9 @@ if (editor.config){
 
     editor.create();
 }
+
+$(".preview_input").change(function(event){
+    var file = event.currentTarget.files[0];
+    var url = window.URL.createObjectURL(file);
+    $(event.target).next(".preview_img").attr("src", url);
+});
