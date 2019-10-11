09a0252b432d2f2b54c73832885ecfa4b84f584a
kwzm <kwzm93@163.com>
2019-10-11 22:35:49 +0800

    add relations in Topic model

--- app/Topic.php
+++ app/Topic.php
@@ -6,5 +6,13 @@ use App\Model;
 
 class Topic extends Model
 {
-    //
+    // 属于这个专题的所有文章
+    public function posts() {
+    	$this->belongsToMany(\App\Post::class, 'post_topics', 'topic_id', 'post_id');
+    }
+
+    // 专题的文章数，用于withCount
+    public function postTopics() {
+    	$this->hasMany(\App\PostTopic::class, 'topic_id');
+    }
 }
