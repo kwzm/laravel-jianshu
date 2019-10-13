0cc449cc816a35eeebd70cab845db52ec09752a0
kwzm <kwzm93@163.com>
2019-10-13 21:38:06 +0800

    add AdminLTE and init login page

--- routes/admin.php
+++ routes/admin.php
@@ -2,5 +2,10 @@
 
 // 管理后台
 Route::group(['prefix' => 'admin'], function(){
-
+	// 登录展示页面
+	Route::get('/login', '\App\Admin\Controllers\LoginController@index');
+	// 登录行为
+	Route::post('/login', '\App\Admin\Controllers\LoginController@login');
+	// 登出行为
+	Route::get('/logout', '\App\Admin\Controllers\LoginController@logout');
 });
