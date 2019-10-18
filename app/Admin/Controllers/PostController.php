d8b808240ab57dd4548f0b178213e042a7910296
kwzm <kwzm93@163.com>
2019-10-18 22:18:52 +0800

    complete admin post status function

--- app/Admin/Controllers/PostController.php
+++ app/Admin/Controllers/PostController.php
@@ -7,10 +7,22 @@ use \App\Post;
 class PostController extends Controller {
 	// 首页
 	public function index() {
-		return view('admin.post.index');
+		$posts = Post::withoutGlobalScope('available')->where('status', 0)->orderBy('created_at', 'desc')->paginate(10);
+
+		return view('admin.post.index', compact('posts'));
 	}
 
 	public function status(Post $post) {
-		// TODO
+		$this->validate(request(), [
+			'status' => 'required|in:-1,1',
+		]);
+
+		$post->status = request("status");
+		$post->save();
+
+		return [
+			'error' => 0,
+			'msg' => ''
+		];
 	}
 }
--- resources/views/admin/post/index.blade.php
+++ resources/views/admin/post/index.blade.php
@@ -13,36 +13,24 @@
                     <!-- /.box-header -->
                     <div class="box-body">
                         <table class="table table-bordered">
-                            <tbody><tr>
-                                <th style="width: 10px">#</th>
-                                <th>文章标题</th>
-                                <th>操作</th>
-                            </tr>
-                                                        <tr>
-                                <td>62.</td>
-                                <td>你的眼，住着天的蓝</td>
-                                <td>
-                                    <button type="button" class="btn btn-block btn-default post-audit" post-id="62" post-action-status="1" >通过</button>
-                                    <button type="button" class="btn btn-block btn-default post-audit" post-id="62" post-action-status="-1" >拒绝</button>
-                                </td>
-                            </tr>
-                                                        <tr>
-                                <td>61.</td>
-                                <td>不要为了“斗茶”，而荒废了喝茶的快乐</td>
-                                <td>
-                                    <button type="button" class="btn btn-block btn-default post-audit" post-id="61" post-action-status="1" >通过</button>
-                                    <button type="button" class="btn btn-block btn-default post-audit" post-id="61" post-action-status="-1" >拒绝</button>
-                                </td>
-                            </tr>
-                                                        <tr>
-                                <td>60.</td>
-                                <td>哪些因素决定文章受欢迎呢？</td>
-                                <td>
-                                    <button type="button" class="btn btn-block btn-default post-audit" post-id="60" post-action-status="1" >通过</button>
-                                    <button type="button" class="btn btn-block btn-default post-audit" post-id="60" post-action-status="-1" >拒绝</button>
-                                </td>
-                            </tr>
-                                                        </tbody></table>
+                            <tbody>
+                                <tr>
+                                    <th style="width: 10px">#</th>
+                                    <th>文章标题</th>
+                                    <th>操作</th>
+                                </tr>
+                                <tr>
+                                    @foreach($posts as $post)
+                                    <td>{{$post->id}}</td>
+                                    <td>{{$post->title}}</td>
+                                    <td>
+                                        <button type="button" class="btn btn-block btn-default post-audit" post-id="{{$post->id}}" post-action-status="1" >通过</button>
+                                        <button type="button" class="btn btn-block btn-default post-audit" post-id="{{$post->id}}" post-action-status="-1" >拒绝</button>
+                                    </td>
+                                    @endforeach
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
 
                 </div>
