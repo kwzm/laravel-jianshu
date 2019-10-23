24de2b4e8f9b714cb493a28d4214c52a292f803c
kwzm <kwzm93@163.com>
2019-10-23 21:26:28 +0800

    add topic routes and views

---
+++ app/Admin/Controllers/TopicController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+class TopicController extends Controller
+{   
+    public function index() {
+        return view('admin/topic/index');
+    } 
+
+
+    public function create() {
+        return view('admin/topic/create');
+    }
+
+    public function store() {
+
+    }
+
+    public function destory() {
+
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -39,5 +39,9 @@ Route::group(['prefix' => 'admin'], function(){
 			Route::get('/posts', '\App\Admin\Controllers\PostController@index');
 			Route::post('/posts/{post}/status', '\App\Admin\Controllers\PostController@status');
 		});
+
+		Route::group(['middleware' => 'can:topic'], function() {
+			Route::resource('topics', '\App\Admin\Controllers\TopicController', ['only' => ['index', 'create', 'store', 'destory']]);
+		});
 	});
 });
